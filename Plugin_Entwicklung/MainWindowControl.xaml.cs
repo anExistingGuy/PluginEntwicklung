e6f8e04c994bc0dd16739008ef76554e9412c913
arljba <arno7@web.de>
2018-07-03 00:48:24 +0200

    Zeilenlänge
    <>

--- Plugin_Entwicklung/MainWindowControl.xaml.cs
+++ Plugin_Entwicklung/MainWindowControl.xaml.cs
@@ -4,7 +4,8 @@
     using Microsoft.CodeAnalysis.Text;
     using Microsoft.VisualStudio.ComponentModelHost;
     using Microsoft.VisualStudio.Shell.Interop;
-    using System.Collections;
+	using System;
+	using System.Collections;
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
@@ -40,6 +41,51 @@
             MessageBox.Show(
                 string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
                 "MainWindow");
-        }
+
+			checklinelenght();
+
+			foreach (var project in projects)
+			{
+				foreach (var document in project.Documents)
+				{
+					System.Diagnostics.Debug.WriteLine("Kappachino" + "\t\t\t" + document.Name);
+					
+				
+				}
+			}
+
+		}
+
+		private void checklinelenght()
+		{
+			IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
+			var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
+
+			var projects = workspace.CurrentSolution.Projects;
+			foreach (var project in projects)
+			{
+				foreach (var document in project.Documents)
+				{
+					if (document != null)
+					{
+						SourceText doctext;
+						document.TryGetText(out doctext);
+						if (doctext != null)
+						{
+							foreach (var line in doctext.Lines)
+							{
+								if (line.ToString().Length > 60)
+								{
+									System.Diagnostics.Debug.WriteLine(document.Name+"Zu lang in Zeile" + line.LineNumber+1);
+								}
+							}
+						}
+
+
+					}
+				}
+			}
+
+		}
     }
 }
