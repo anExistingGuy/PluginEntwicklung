bf2a844675600366052e8fd69dedb7f6d18e9214
anExistingGuy <benjamin-pfaff@web.de>
2018-07-04 01:27:36 +0200

    Struktur

---
+++ Plugin_Entwicklung/Controller/LineLengthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_Entwicklung.Controller
+{
+    class LineLengthController
+    {
+        // public void CheckLineLength(VisualStudioWorkspace workspace)
+        public void CheckLineLength(Project project, int numChars)
+        {
+            IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
+            var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
+
+            foreach (var document in project.Documents)
+            {
+                if (document != null)
+                {
+                    SourceText doctext;
+                    document.TryGetText(out doctext);
+                    if (doctext != null)
+                    {
+                        foreach (var line in doctext.Lines)
+                        {
+                            if (line.ToString().Length > numChars)
+                            {
+                                System.Diagnostics.Debug.WriteLine(document.Name + "Zu lang in Zeile" + line.LineNumber + 1);
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
--- Plugin_Entwicklung/MainWindowControl.xaml.cs
+++ Plugin_Entwicklung/MainWindowControl.xaml.cs
@@ -3,9 +3,11 @@
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.Text;
     using Microsoft.VisualStudio.ComponentModelHost;
+    using Microsoft.VisualStudio.LanguageServices;
     using Microsoft.VisualStudio.Shell.Interop;
-	using System;
-	using System.Collections;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
@@ -15,12 +17,29 @@
     /// </summary>
     public partial class MainWindowControl : UserControl
     {
+        // List<Project> Projects { get; set; }
+        public ObservableCollection<Project> Projects { get; set; }
+        public Project SelectedProject { get; set; }
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowControl"/> class.
         /// </summary>
         public MainWindowControl()
         {
             this.InitializeComponent();
+            // Projects = new List<Project>();
+            Projects = new ObservableCollection<Project>();
+
+            IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
+            var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
+
+            var projects = workspace.CurrentSolution.Projects;
+            foreach (Project p in projects)
+            {
+                this.Projects.Add(p);
+            }
+
+            DataContext = this;
         }
 
         /// <summary>
@@ -30,62 +49,27 @@
         /// <param name="e">The event args.</param>
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void CheckProject(object sender, RoutedEventArgs e)
         {
-            
-            IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
-            var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
-
-            var projects = workspace.CurrentSolution.Projects;
-
             MessageBox.Show(
                 string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
                 "MainWindow");
 
-			checklinelenght();
+            new Controller.LineLengthController().CheckLineLength(this.SelectedProject, int.Parse(this.textBox_lengthLines.ToString()));
+        }
 
-			foreach (var project in projects)
-			{
-				foreach (var document in project.Documents)
-				{
-					System.Diagnostics.Debug.WriteLine("Kappachino" + "\t\t\t" + document.Name);
-					
-				
-				}
-			}
+        private void RefreshWorkspace(object sender, RoutedEventArgs e)
+        {
+            IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
+            var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
+            
+            var projects = workspace.CurrentSolution.Projects;
 
-		}
-
-		private void checklinelenght()
-		{
-			IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
-			var workspace = componentModel.GetService<Microsoft.VisualStudio.LanguageServices.VisualStudioWorkspace>();
-
-			var projects = workspace.CurrentSolution.Projects;
-			foreach (var project in projects)
-			{
-				foreach (var document in project.Documents)
-				{
-					if (document != null)
-					{
-						SourceText doctext;
-						document.TryGetText(out doctext);
-						if (doctext != null)
-						{
-							foreach (var line in doctext.Lines)
-							{
-								if (line.ToString().Length > 60)
-								{
-									System.Diagnostics.Debug.WriteLine(document.Name+"Zu lang in Zeile" + line.LineNumber+1);
-								}
-							}
-						}
-
-
-					}
-				}
-			}
-
-		}
+            this.Projects.Clear();
+            foreach (Project p in projects)
+            {
+                this.Projects.Add(p);
+            }
+        }
     }
 }
