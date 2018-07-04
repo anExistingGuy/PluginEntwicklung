4f91b5cffd12b90d4aedd6cdf6f67ebcb2d00393
arljba <arno7@web.de>
2018-07-04 17:50:27 +0200

    Namechecking für Methoden/Klassen/Variablen/Namespaces
    <>

--- Plugin_Entwicklung/Controller/NamingController.cs
+++ Plugin_Entwicklung/Controller/NamingController.cs
@@ -20,22 +20,64 @@ namespace Plugin_Entwicklung.Controller
             {
                 if (document != null)
                 {
-                    SourceText doctext;
-                    document.TryGetText(out doctext);
-                    if (doctext != null)
-                    {
                         //SyntaxTree tree = CSharpSyntaxTree.ParseText(doctext);
                         SyntaxTree tree;
                         Task<SyntaxTree> t = document.GetSyntaxTreeAsync();
                         tree = t.Result;
-                        List<MethodDeclarationSyntax>list = tree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
-                        list.ForEach(delegate (MethodDeclarationSyntax mds) 
-                        {
-                             System.Diagnostics.Debug.WriteLine("Methodenname: " +mds.Identifier);
-                        });
-                    }
+						if (true)
+						{
+							List<MethodDeclarationSyntax> list = tree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
+							methodnaming(list);
+						}
+						if (true)
+						{
+							List<ClassDeclarationSyntax> list = tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+							classnaming(list);
+						}
+						if (true)
+						{
+							List<VariableDeclaratorSyntax> list = tree.GetRoot().DescendantNodes().OfType<VariableDeclaratorSyntax>().ToList();
+							variablenaming(list);
+						}
+						if (true)
+						{
+							List<NamespaceDeclarationSyntax> list = tree.GetRoot().DescendantNodes().OfType<NamespaceDeclarationSyntax>().ToList();
+							namespacenaming(list);
+						}
                 }
             }
         }
-    }
+
+		public void methodnaming(List<MethodDeclarationSyntax> list)
+		{
+			list.ForEach(delegate (MethodDeclarationSyntax mds)
+			{
+				System.Diagnostics.Debug.WriteLine("Methodenname: " + mds.Identifier);
+			});
+		}
+
+		public void classnaming(List<ClassDeclarationSyntax> list)
+		{
+			list.ForEach(delegate (ClassDeclarationSyntax cds)
+			{
+				System.Diagnostics.Debug.WriteLine("Klassenname: " + cds.Identifier);
+			});
+		}
+
+		public void variablenaming(List<VariableDeclaratorSyntax> list)
+		{
+			list.ForEach(delegate (VariableDeclaratorSyntax vds)
+			{
+				System.Diagnostics.Debug.WriteLine("Variablenname: " + vds.Identifier);
+			});
+		}
+
+		public void namespacenaming(List<NamespaceDeclarationSyntax> list)
+		{
+			list.ForEach(delegate (NamespaceDeclarationSyntax nds)
+			{
+				System.Diagnostics.Debug.WriteLine("Namespacename: " + nds.Name);
+			});
+		}
+	}
 }
