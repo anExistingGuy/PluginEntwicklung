8769cf28c8bc1216391271eaca4b162ad8e3a8d1
arljba <arno7@web.de>
2018-07-07 00:28:49 +0200

    MVVM Angefangen
    <>

--- Plugin_Entwicklung/Controller/MVVMController.cs
+++ Plugin_Entwicklung/Controller/MVVMController.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Plugin_Entwicklung.Controller
 {
-    class MVVMController
-    {
-    }
+	class MVVMConntroller
+	{
+		public void CheckMVVM(ObservableCollection<Document> modeldocuments, ObservableCollection<Document> viewdocuments, ObservableCollection<Document> viewmodeldocuments)
+		{
+			foreach (Document d in modeldocuments)
+			{
+				Task<SyntaxTree> t = d.GetSyntaxTreeAsync();
+				SyntaxTree tree = t.Result;
+				List<MethodDeclarationSyntax> methodDeclarationsinModel =
+					tree.GetRoot().DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>().ToList();
+			}
+
+			foreach (Document d in viewdocuments)
+			{
+				Task<SyntaxTree> t = d.GetSyntaxTreeAsync();
+				SyntaxTree tree = t.Result;
+				List<MethodDeclarationSyntax> methodDeclarationsinModel =
+					tree.GetRoot().DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>().ToList();
+			}
+
+			foreach (Document d in viewmodeldocuments)
+			{
+				Task<SyntaxTree> t = d.GetSyntaxTreeAsync();
+				SyntaxTree tree = t.Result;
+				List<MethodDeclarationSyntax> methodDeclarationsinModel =
+					tree.GetRoot().DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>().ToList();
+			}
+		}
+	}
 }
