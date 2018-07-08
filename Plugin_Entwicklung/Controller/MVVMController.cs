8e42f9bdc80b1e5da0441019038b75fd119b0acc
arljba <arno7@web.de>
2018-07-08 17:13:04 +0200

    MVVM Kommentare
    <>

--- Plugin_Entwicklung/Controller/MVVMController.cs
+++ Plugin_Entwicklung/Controller/MVVMController.cs
@@ -11,7 +11,8 @@ namespace Plugin_Entwicklung.Controller
 {
 	class MVVMConntroller
 	{
-		public void CheckMVVM(Project project, ObservableCollection<Document> modeldocuments, ObservableCollection<Document> viewdocuments, ObservableCollection<Document> viewmodeldocuments)
+		//The CheckMVVM Method which is called from the MainWindowControl.xaml.cs when the assigned button is pressed
+		public void CheckMVVM(Project project, ObservableCollection<Document> modelDocuments, ObservableCollection<Document> viewDocuments, ObservableCollection<Document> viewmodelDocuments)
 		{
 			List<ClassDeclarationSyntax> classDeclarationsinModel = null;
 			List<ClassDeclarationSyntax> classDeclarationsinView = null;
@@ -19,15 +20,16 @@ namespace Plugin_Entwicklung.Controller
 			List<ObjectCreationExpressionSyntax> idDeclarationsinModel = null;
 			List<ObjectCreationExpressionSyntax> idDeclarationsinView = null;
 
+			//create a list of tuples so the document can still be accesed later on
             List<Tuple<ObjectCreationExpressionSyntax, Document>> idDeclarationsModel = new List<Tuple<ObjectCreationExpressionSyntax, Document>>();
             List<Tuple<ObjectCreationExpressionSyntax, Document>> idDeclarationsView = new List<Tuple<ObjectCreationExpressionSyntax, Document>>();
 
-			foreach (Document d in modeldocuments)
+			foreach (Document d in modelDocuments)
 			{
 				Task<SyntaxTree> t = d.GetSyntaxTreeAsync();
 				SyntaxTree tree = t.Result;
 
-
+				//get the classdeclaration of the model documents and a list of the objects that are created within it
 				classDeclarationsinModel =
 					tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>().ToList();
 				idDeclarationsinModel =
@@ -39,11 +41,12 @@ namespace Plugin_Entwicklung.Controller
                 }
 			}
 
-			foreach (Document d in viewdocuments)
+			foreach (Document d in viewDocuments)
 			{
 				Task<SyntaxTree> t = d.GetSyntaxTreeAsync();
 				SyntaxTree tree = t.Result;
 
+				//get the classdeclaration of the view documents and a list of the objects that are created within it
 				classDeclarationsinView =
 					tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>().ToList();
 				idDeclarationsinView =
@@ -59,29 +62,32 @@ namespace Plugin_Entwicklung.Controller
 			CheckCodeBehind(project);
 		}
 
-		public void ModelknowsViews(List<ClassDeclarationSyntax> classmodellist, List<ClassDeclarationSyntax> classviewlist,
-			List<Tuple<ObjectCreationExpressionSyntax, Document>> idmodellist, List<Tuple<ObjectCreationExpressionSyntax, Document>> idviewlist)
+		/*this method checks whether the view knows the model or the other way around. This will be the case
+		 * if an object of the other is instantiated anywhere within it.
+		*/
+		public void ModelknowsViews(List<ClassDeclarationSyntax> classModellist, List<ClassDeclarationSyntax> classViewlist,
+			List<Tuple<ObjectCreationExpressionSyntax, Document>> idModellist, List<Tuple<ObjectCreationExpressionSyntax, Document>> idViewlist)
 		{
-			classmodellist.ForEach(delegate (ClassDeclarationSyntax cdsm)
+			classModellist.ForEach(delegate (ClassDeclarationSyntax cdsm)
 			{
-				string modelclassname = cdsm.Identifier.ToString();
-				idviewlist.ForEach(delegate (Tuple<ObjectCreationExpressionSyntax, Document> tuple)
+				string modelClassname = cdsm.Identifier.ToString();
+				idViewlist.ForEach(delegate (Tuple<ObjectCreationExpressionSyntax, Document> tuple)
 				{
-					string createdobject = tuple.Item1.ToString();
-					if (createdobject.Contains(modelclassname))
+					string createdObject = tuple.Item1.ToString();
+					if (createdObject.Contains(modelClassname))
 					{
                         ErrorReporter.AddWarning("Model accessing View!", tuple.Item2.FilePath);
 					}
 				});
 			});
 
-			classviewlist.ForEach(delegate (ClassDeclarationSyntax cdsv)
+			classViewlist.ForEach(delegate (ClassDeclarationSyntax cdsv)
 			{
-				string viewclassname = cdsv.Identifier.ToString();
-				idmodellist.ForEach(delegate (Tuple<ObjectCreationExpressionSyntax, Document> tuple)
+				string viewClassname = cdsv.Identifier.ToString();
+				idModellist.ForEach(delegate (Tuple<ObjectCreationExpressionSyntax, Document> tuple)
 				{
-					string createdobject = tuple.Item1.ToString();
-					if (createdobject.Contains(viewclassname))
+					string createdObject = tuple.Item1.ToString();
+					if (createdObject.Contains(viewClassname))
 					{
                         ErrorReporter.AddWarning("View accessing model!", tuple.Item2.FilePath);
 					}
@@ -89,6 +95,9 @@ namespace Plugin_Entwicklung.Controller
 			});
 		}
 
+		/*this method checks if the code behind file is left empty. Every code behind file has a .xaml as its parent
+		 * which is treatet like its folder.
+		 */
 		public void CheckCodeBehind(Project project)
 		{
 			foreach (var document in project.Documents)
@@ -98,6 +107,7 @@ namespace Plugin_Entwicklung.Controller
 					Task<SyntaxTree> t = document.GetSyntaxTreeAsync();
 					SyntaxTree tree = t.Result;
 
+					// get every method/variable and propertydeclaration of the current document
 					List<MethodDeclarationSyntax> methodDeclarations =
 						tree.GetRoot().DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>().ToList();
 					List<VariableDeclarationSyntax> variableDeclarations =
@@ -106,9 +116,10 @@ namespace Plugin_Entwicklung.Controller
 						tree.GetRoot().DescendantNodesAndSelf().OfType<PropertyDeclarationSyntax>().ToList();
 
 					List<string> folders = document.Folders.ToList();
-					folders.ForEach(delegate (string fldr)
+					folders.ForEach(delegate (string folder)
 					{
-						if (fldr.Contains(".xaml")&& (methodDeclarations.Any()||variableDeclarations.Any()||propertyDeclarations.Any()))
+						//check if any declarations are in the code behind file
+						if (folder.Contains(".xaml")&& (methodDeclarations.Any()||variableDeclarations.Any()||propertyDeclarations.Any()))
 						{
 							System.Diagnostics.Debug.WriteLine("Codebehind sollte Leer sein");
                             ErrorReporter.AddWarning("Codebehind is not empty!", document.FilePath);
